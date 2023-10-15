2e7de3c697ebeac2ea868a75509f08d654635241
catsagainst <danilova.maria.k@gmail.com>
2023-10-15 16:44:53 +0200

    add test_addToCartAndCheckout.py

--- Test_QAA/tests/test_addToCartAndCheckout.py
+++ Test_QAA/tests/test_addToCartAndCheckout.py
@@ -24,6 +24,29 @@ def test_addToCart():
     cart_button = driver.find_element(By.XPATH, '//span[@class ="shopping_cart_badge"]')
     cart_button.click()
 
+    text_before = driver.find_element(By.CSS_SELECTOR, 'a[id="item_4_title_link"] > div[class="inventory_item_name"]')
+
+
+    text_after = driver.find_element(By.CSS_SELECTOR, 'a[id="item_4_title_link"] > div[class="inventory_item_name"]')
+
+    assert text_before==text_after
+
+    time.sleep(5)
+
+    assert driver.find_elements(By.XPATH, "//div[@class='cart_quantity'][contains(text(),'1')]")
+
+    checkout_button = driver.find_element(By.XPATH, '//button[@id="checkout"]')
+    checkout_button.click()
+
+
+    assert driver.current_url == "https://www.saucedemo.com/checkout-step-one.html"
+    driver.quit()
+
+
+
+    cart_button = driver.find_element(By.XPATH, '//span[@class ="shopping_cart_badge"]')
+    cart_button.click()
+
     time.sleep(5)
 
     assert driver.find_elements(By.XPATH, "//div[@class='cart_quantity'][contains(text(),'1')]")
